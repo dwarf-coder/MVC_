838114b13d75b5e8d4e5d468e11b69524c7d7290
dwarf-coder <66822259+dwarf-coder@users.noreply.github.com>
2020-10-07 07:44:25 -0500

    Add files via upload

---
+++ MVC/intern.java
@@ -0,0 +1,22 @@
+public class Intern {
+   private String name;
+   private String lastname;
+   private String city;
+   private String phone;
+   
+   public String getName() {
+      return name;
+   }
+   
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getLastname() {
+      return lastname;
+   }
+   
+   public void setLastname(String lastname) {
+      this.lastname = lastname;
+   }
+}
---
+++ MVC/internView.java
@@ -0,0 +1,7 @@
+public class InternView {
+    public void printInternDetails(String internName, String internLastname){
+       System.out.println("Intern: ");
+       System.out.println("Name: " + internName);
+       System.out.println("Lastname: " + internLastname);
+    }
+ }
