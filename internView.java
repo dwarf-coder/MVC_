e8e989058d53c88364f40568df85c57c7a3f25f1
dwarf-coder <66822259+dwarf-coder@users.noreply.github.com>
2020-10-07 07:49:06 -0500

    Delete internView.java

--- internView.java
+++
@@ -1,7 +0,0 @@
-public class InternView {
-    public void printInternDetails(String internName, String internLastname){
-       System.out.println("Intern: ");
-       System.out.println("Name: " + internName);
-       System.out.println("Lastname: " + internLastname);
-    }
- }
